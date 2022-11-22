f7d13ccca4b88c8db30f740d7d3040137a7d1ffd
Lex <maskoLex@yandex.ru>
2022-11-22 16:26:46 +0300

    work 22.11.22

--- blog/deflor/forms.py
+++ blog/deflor/forms.py
@@ -14,3 +14,10 @@ class AddWomen(ModelForm):
             'photo': FileInput(attrs={'class': 'form-control'}),
             'cat': Select(attrs={'class': 'form-select form-select-sm', 'aria-label': '.form-select-sm example'}),
         }
+
+    # Валидация данных
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError("Длинна строки больше 200 символов")
+        return title
